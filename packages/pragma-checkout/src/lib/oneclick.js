d1015966ce75cd694c80feb3d8f378bc956a23e7
Aagam Jain <aagam@logisy.tech>
2024-07-24 14:22:18 +0530

    added: Customer login

--- packages/pragma-checkout/src/lib/oneclick.js
+++ packages/pragma-checkout/src/lib/oneclick.js
@@ -1,56 +1,59 @@
-import axios from 'axios';
-import { encode } from 'base-64';
+  import axios from 'axios';
+  import { encode } from 'base-64';
+  import { getUser } from '@appmaker-xyz/core';
 
-export const getOneClickCheckoutUrl = async (checkoutObject) => {
-  const API_KEY_1C = 'a2b4e755-6990-4fa8-a266-7c44a188df1e';
-  let encodedResponse = '';
-  let appmakerObject = { appmaker: true, ...checkoutObject };
-  await axios
-    .post(
-      'https://api.1checkout.ai/create',
-      JSON.stringify(appmakerObject),
-      {
-        headers: {
-          'Content-type': 'application/json',
-          'X-LOGISY-API-KEY': API_KEY_1C,
+  export const getOneClickCheckoutUrl = async (checkoutObject) => {
+    const API_KEY_1C = 'a2b4e755-6990-4fa8-a266-7c44a188df1e';
+    const user = getUser();
+    let encodedUser = encode(JSON.stringify(user))
+    let encodedResponse = '';
+    let appmakerObject = { appmaker: true, ...checkoutObject };
+    await axios
+      .post(
+        'https://api.1checkout.ai/create',
+        JSON.stringify(appmakerObject),
+        {
+          headers: {
+            'Content-type': 'application/json',
+            'X-LOGISY-API-KEY': API_KEY_1C,
+          },
         },
-      },
-    )
-    .then((response) => {
-      // Handle successful response
-      const message = {
-        checkout_id: response.data.checkout_id,
-        checkout_url: response.data.checkout_url,
-        notes: null,
+      )
+      .then((response) => {
+        // Handle successful response
+        const message = {
+          checkout_id: response.data.checkout_id,
+          checkout_url: response.data.checkout_url,
+          notes: null,
+        };
+        encodedResponse = encode(JSON.stringify(message));
+      })
+      .catch((error) => {
+        // Handle error
+        console.error('An error occurred:', error);
+      });
+
+    const url = `https://pay.1checkout.ai/v4/checkout/widget/?showerror=false&checkout_type=checkout&containerheight=720&apikey=${API_KEY_1C}&response=${encodedResponse}&user=${encodedUser}`;
+    return url;
+  };
+
+  // you can filter the urls that you get in checkout page here
+  export const customPaymentSchema = {
+    id: 'oneclick-payment-schema',
+    regex: [
+      /phonepe:\/\/pay\?.*/i,
+      /paytm:\/\/upi\/pay?.*/i,
+      /paytmmp:\/\/pay?.*/i,
+      /upi:\/\/pay?.*/i,
+      /tez:\/\/upi\/pay?.*/i,
+      /.*\.razorpay\.com*/i,
+    ],
+    action: (url) => {
+      return {
+        action: 'OPEN_URL',
+        params: {
+          url,
+        },
       };
-      encodedResponse = encode(JSON.stringify(message));
-    })
-    .catch((error) => {
-      // Handle error
-      console.error('An error occurred:', error);
-    });
-
-  const url = `https://pay.1checkout.ai/v4/checkout/widget/?showerror=false&checkout_type=checkout&containerheight=720&apikey=${API_KEY_1C}&response=${encodedResponse}`; // replace this with your url
-  return url;
-};
-
-// you can filter the urls that you get in checkout page here
-export const customPaymentSchema = {
-  id: 'oneclick-payment-schema',
-  regex: [
-    /phonepe:\/\/pay\?.*/i,
-    /paytm:\/\/upi\/pay?.*/i,
-    /paytmmp:\/\/pay?.*/i,
-    /upi:\/\/pay?.*/i,
-    /tez:\/\/upi\/pay?.*/i,
-    /.*\.razorpay\.com*/i,
-  ],
-  action: (url) => {
-    return {
-      action: 'OPEN_URL',
-      params: {
-        url,
-      },
-    };
-  },
-};
+    },
+  };
